4fe42f90b22a26c21a5d3d13b6a5183e8b21767e
Hrvoje Fekete <hrvoje.fekete@gmail.com>
2023-09-21 10:37:20 -0700

    Add Individual person.

--- btr-web/btr-main-app/pages/index.vue
+++ btr-web/btr-main-app/pages/index.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>
+  <div class="mx-10">
+    <div class="mx-5 my-5 p-10 bg-white rounded flex">
+      <div class="p-5 flex-none w-1/5 col-auto">
+        <span class="font-bold">Add an individual person</span>
+      </div>
+      <div class="p-5 flex-none w-4/5">
+        <AddIndividualPerson />
+      </div>
+
+    </div>
     <NUIndividualPersonSummaryTable :individuals="individuals" />
     <br>
     <IndividualPersonSummaryTable :individual-persons="individuals" />
